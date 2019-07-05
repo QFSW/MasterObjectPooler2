f2325848b29bd2bd126b3691be3b2690848f6c5e
QFSW <yusuf.ismail1024@gmail.com>
2019-07-05 17:46:42 +0100

    More work on banner and custom editor

---
+++ Source/Editor/EditorHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QFSW.MOP2.Editor
+{
+    public static class EditorHelpers
+    {
+        public static void DrawBanner(Texture2D banner)
+        {
+            if (banner)
+            {
+                Rect bannerRect = GUILayoutUtility.GetRect(0.0f, 0.0f);
+                bannerRect.height = Screen.width * banner.height / banner.width;
+                GUILayout.Space(bannerRect.height);
+                GUI.Label(bannerRect, banner);
+            }
+        }
+    }
+}
---
+++ Source/Editor/ObjectPoolInspector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace QFSW.MOP2.Editor
+{
+    [CustomEditor(typeof(ObjectPool))]
+    public class ObjectPoolInspector : UnityEditor.Editor
+    {
+        [SerializeField] private Texture2D _banner = null;
+
+        public override void OnInspectorGUI()
+        {
+            EditorHelpers.DrawBanner(_banner);
+            base.OnInspectorGUI();
+        }
+    }
+}
