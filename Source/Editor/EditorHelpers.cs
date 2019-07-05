dc8e6e987851db090741ec2c109fdfe177c3c3ca
QFSW <yusuf.ismail1024@gmail.com>
2019-07-05 18:27:54 +0100

    Finished banner

--- Source/Editor/EditorHelpers.cs
+++ Source/Editor/EditorHelpers.cs
@@ -6,12 +6,18 @@ namespace QFSW.MOP2.Editor
 {
     public static class EditorHelpers
     {
-        public static void DrawBanner(Texture2D banner)
+        public static void DrawBanner(Texture2D banner, float sizeMultiplier = 1f)
         {
             if (banner)
             {
+                sizeMultiplier = Mathf.Clamp01(sizeMultiplier);
+
                 Rect bannerRect = GUILayoutUtility.GetRect(0.0f, 0.0f);
                 bannerRect.height = Screen.width * banner.height / banner.width;
+                bannerRect.x += bannerRect.width * (1 - sizeMultiplier) / 2;
+                bannerRect.width *= sizeMultiplier;
+                bannerRect.height *= sizeMultiplier;
+
                 GUILayout.Space(bannerRect.height);
                 GUI.Label(bannerRect, banner);
             }
