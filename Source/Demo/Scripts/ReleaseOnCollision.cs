2b788e27ca3f5d476d2f9a330d5483c2e3b2887f
QFSW <yusuf.ismail1024@gmail.com>
2019-07-12 10:11:41 +0100

    Reorganised demo stuff

---
+++ Source/Demo/Scripts/ReleaseOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace QFSW.MOP2.Demo
+{
+    [RequireComponent(typeof(Collider))]
+    public class ReleaseOnCollision : PoolableMonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            Release();
+        }
+    }
+}
