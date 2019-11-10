0d6cb293ede1b91e752599b8616a383e3dc091c7
Yusuf Ismail <yusuf@qfsw.co.uk>
2019-11-10 13:58:06 +0000

    Added particle effects to the demo scene

--- Source/Demo/Scripts/ReleaseOnCollision.cs
+++ Source/Demo/Scripts/ReleaseOnCollision.cs
@@ -6,12 +6,17 @@ namespace QFSW.MOP2.Demo
     public class ReleaseOnCollision : PoolableMonoBehaviour
     {
         [SerializeField] private LayerMask _collisionLayer = 0;
+        [SerializeField] private ObjectPool _deathFX = null;
 
         private void OnCollisionEnter(Collision collision)
         {
             if ((_collisionLayer.value & 1 << collision.gameObject.layer) != 0)
             {
                 Release();
+                if (_deathFX)
+                {
+                    _deathFX.GetObject(transform.position);
+                }
             }
         }
     }
